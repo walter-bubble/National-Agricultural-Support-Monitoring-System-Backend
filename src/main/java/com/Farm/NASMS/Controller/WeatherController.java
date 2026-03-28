297d608e3903bb33b466382e179023850189b23a
walter-bubble <munenewalter486@gmail.com>
2026-03-28 22:14:19 +0300

    28/3 update2

--- src/main/java/com/Farm/NASMS/Controller/WeatherController.java
+++ src/main/java/com/Farm/NASMS/Controller/WeatherController.java
@@ -1,4 +1,19 @@
 package com.Farm.NASMS.Controller;
 
+import com.Farm.NASMS.Service.WeatherService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/weather")
 public class WeatherController {
+    private WeatherService weatherService;
+    public WeatherController(WeatherService weatherService){
+        this.weatherService=weatherService;
+    }
+    @GetMapping
+    public String getWeather(@RequestParam String city){
+        return weatherService.getWeatherByCity(city);
+    }
+    
+
 }
