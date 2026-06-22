a4b3aadf78f50433e73e71d8430830be6b97509a
walter-bubble <munenewalter486@gmail.com>
2026-06-22 12:08:00 +0300

    jk testing

--- src/main/java/com/Farm/NASMS/NasmsApplication.java
+++ src/main/java/com/Farm/NASMS/NasmsApplication.java
@@ -3,8 +3,6 @@ package com.Farm.NASMS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
 @EnableScheduling
 @SpringBootApplication
 public class NasmsApplication {
